d8ec1275b51eec7abb775fcfc57bc0c2fd2d1dcb
LUIS MIGUEL CARDONA MENESES <luiscardona316167@correo.itm.edu.co>
2025-11-12 17:19:18 -0500

    Add project files.

---
+++ API.P.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.P.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.P.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options) // Metodo constructor que tiene el mismo nombre que la clase, <> notacion diamante
+        {
+            
+        }
+
+        //Sección para crear el dbset de las entidades o modelos
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+}
---
+++ API.P.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.P.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key] //Decorator
+        // Este data annotation indica que esta propiedad es la clave primaria
+        // The virtual is like a inherits (herencia) 
+        public virtual int Id { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; } 
+
+        public virtual DateTime ModifiedDate { get; set; } 
+    }
+}
