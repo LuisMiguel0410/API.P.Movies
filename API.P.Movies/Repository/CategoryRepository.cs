9a961bf2ac145e12a298b57edc77b0c041b79fd6
LUIS MIGUEL CARDONA MENESES <luiscardona316167@correo.itm.edu.co>
2025-11-13 17:06:46 -0500

    New CategoryRepository created

---
+++ API.P.Movies/Repository/CategoryRepository.cs
@@ -0,0 +1,80 @@
+﻿using API.P.Movies.DAL;
+using API.P.Movies.DAL.Models;
+using API.P.Movies.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.P.Movies.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        public async Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .AnyAsync(c => c.Id == id); // esta linea significa que almenos un registro con ese Id existe, debuelve true o false
+
+        }
+
+        public async Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            return await _context.Categories
+            .AsNoTracking()
+            .AnyAsync(c => c.Name == name);
+        }
+
+        public async Task<bool> CreateCategoryAsync(Category category)
+        {
+            category.CreatedDate = DateTime.UtcNow;
+            await _context.Categories.AddAsync(category);
+            return await SaveAsync(); // Cuando inserto algo en base de datos debo llamar a este metodo SaveChangesAsync
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            var category = await GetCategoryAsync(id); // primero busco que si exista la categoria
+
+            if (category == null)
+            {
+                return false; //la categoria no existe
+            }
+
+            _context.Categories.Remove(category); // si existe la elimino
+            return await SaveAsync();
+            //sql Delete from Categories where Id = id
+        }
+
+        public async Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories
+            .AsNoTracking()
+            .OrderBy(c => c.Name)  //Ascending order, para ordenar descending usar .OrderByDescending
+            .ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryAsync(int id) // palabras claves async y el await
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id); //lambda expression (Select * from Categories WHERE Id = id)
+        }
+
+        public async Task<bool> UpdateCategoryAsync(Category category)
+        {
+            category.ModifiedDate = DateTime.UtcNow;
+            _context.Categories.Update(category);
+            return await SaveAsync();
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0 ? true : false;
+            // condicional ternario, si es mayor o igual a 0 devuelve true, sino false, el ? hace de entonces y el : hace de sino
+        }
+    }
+}
---
+++ API.P.Movies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using API.P.Movies.DAL.Models;
+
+namespace API.P.Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); //Me retorna una lista de categorias
+        Task<Category> GetCategoryAsync(int id); //Me retorna una categoria por su Id
+        Task<bool> CategoryExistsByIdAsync(int id); //Me dice si una categoria existe por su Id
+        Task<bool> CategoryExistsByNameAsync(string name); //Me dice si una categoria existe por su nombre
+        Task<bool> CreateCategoryAsync(Category category); //Me crea una categoria
+        Task<bool> UpdateCategoryAsync(Category category); //Me actualiza una categoria, puedo actualizar el nombre y la fecha de actualizacion
+        Task<bool> DeleteCategoryAsync(int id); //Me elimina una categoria
+        // Estas son las 7 firmas de los metodos
+    }
+}
