e90982eaafceca2ed398cc87ed655eaf67c593e0
LUIS MIGUEL CARDONA MENESES <luiscardona316167@correo.itm.edu.co>
2025-11-14 21:32:42 -0500

    New CategoriesController and CategorySerices created

---
+++ API.P.Movies/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using API.P.Movies.DAL.Models.Dtos;
+using API.P.Movies.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.P.Movies.Controllers
+{
+    //controladores 
+    [Route("api/[controller]")] //ruta o URL para poder navegar o hacer las consultas
+    [ApiController]// Sin este no se puede convertir un controlador en una API
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)] //indica que el resultado esperado es un 200 OK
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] 
+        public async Task<ActionResult<ICollection<CategoryDto>>> GetCategoriesAsync()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            return Ok(categories); // Ok significa que la respuesta fue exitosa, http status code 200
+        }
+    }
+}
